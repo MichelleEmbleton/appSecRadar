c73b09d8b16c3e53252d6f8ae5f785d910b0965e
MichelleEmbleton <michellelembleton@gmail.com>
2017-07-02 18:53:39 +0100

    Create legend.js

---
+++ js/legend.js
@@ -0,0 +1,10 @@
+
+<script>
+function showLegend() {
+	document.getElementById("legend").className = "show-legend";
+	}
+function hideLegend() {
+	document.getElementById("legend").className = "hide-legend";
+	}
+</script>
+          
