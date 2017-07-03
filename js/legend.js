d803accf19a9bcb3cf4bf0ed6dfdfe280ba62edf
MichelleEmbleton <michellelembleton@gmail.com>
2017-07-03 13:13:34 +0100

    Update legend.js

--- js/legend.js
+++ js/legend.js
@@ -1,10 +1,6 @@
-
-<script>
 function showLegend() {
 	document.getElementById("legend").className = "show-legend";
 	}
 function hideLegend() {
 	document.getElementById("legend").className = "hide-legend";
-	}
-</script>
-          
+	}          
