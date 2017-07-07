6a15b844515a45e80df0b85143cc1f7e76a577d4
MichelleEmbleton <michellelembleton@gmail.com>
2017-07-07 13:38:06 +0100

    Delete legend.js

--- js/legend.js
+++
@@ -1,6 +0,0 @@
-function showLegend() {
-	document.getElementById("legend").className = "show-legend";
-	}
-function hideLegend() {
-	document.getElementById("legend").className = "hide-legend";
-	}          
