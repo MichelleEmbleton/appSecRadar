d3f49e409e42b64ee572ee26d4fe9d7f4365861b
MichelleEmbleton <michellelembleton@gmail.com>
2019-05-21 19:31:08 +0100

    Delete detailsPopup.js

--- src/js/elements/detailsPopup.js
+++
@@ -1,8 +0,0 @@
-import { renderDetailsPopup } from './detailsPopupView';
-import { fadeIn } from '../transitions';
-import { doms } from '../base';
-
-export const detailsPopupControl = (id, data) => {
-    data.forEach(el => id === el.TECH && renderDetailsPopup(el));
-    fadeIn(doms.detailsBox);
-};
