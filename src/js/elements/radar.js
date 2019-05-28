5f86ab388f74e89344e0075bdfac20a1fb3523f1
MichelleEmbleton <michellelembleton@gmail.com>
2019-05-28 11:05:37 +0100

    Update radar.js

--- src/js/elements/radar.js
+++ src/js/elements/radar.js
@@ -76,13 +76,11 @@ export const calcSectors = (sectors, data, equal=true, init=true) => {
         let maxA = equal ? equalAngles * (i + 1) : counts[i].max;
         const radA = Number(((minA) * (Math.PI / 180)).toFixed(4)); 	
 		const radAB = Number(((maxA) * (Math.PI / 180)).toFixed(4));
-		const x = 330 + Math.round(r * Math.cos(radA));	
-		const y = 330 + Math.round(r * Math.sin(radA));	
-		const xa = 330 + Math.round((r+3) * Math.cos(radA));
-		const ya = 330 + Math.round((r+3) * Math.sin(radA));
-		const xb = 330 + Math.round((r+3) * Math.cos(radAB));
-		const yb = 330 + Math.round((r+3) * Math.sin(radAB));
-		const sectorLine = `M330 330 L ${x}, ${y}`;
+		const xa = 330 + Math.round(r * Math.cos(radA));
+		const ya = 330 + Math.round(r * Math.sin(radA));
+		const xb = 330 + Math.round(r * Math.cos(radAB));
+		const yb = 330 + Math.round(r * Math.sin(radAB));
+		const sectorLine = `M330 330 L ${xa}, ${ya}`;
 		const sectorArc = `M ${xa}, ${ya} A300, 300, 0, 0, 1 ${xb}, ${yb}`;
 		let sectorConfig = [sectors[i], radA, radAB];    
         setAngleLimits(sectorConfig, data);
