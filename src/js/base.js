bab1abaf9a63121c1176c142c165e4f82f83239a
MichelleEmbleton <michellelembleton@gmail.com>
2019-06-01 11:41:40 +0100

    Update base.js

--- src/js/base.js
+++ src/js/base.js
@@ -6,8 +6,6 @@ export const doms = {
     legendContent: document.getElementById('legend_content'),
     anomalyTable: document.getElementById('anomaly_table'),
     modeChart: document.getElementById('mode_chart'),
-    statusModeList: document.getElementById('status_mode_list'),
-    subcatModeList: document.getElementById('subcat_mode_list'),
     detailsBox: document.getElementById("details_box"),
     sectorList: document.getElementById("sector_list"),
     statusList: document.getElementById("status_list"),
@@ -19,5 +17,12 @@ export const svgs = {
     xs: "http://www.w3.org/1999/xlink"
 };
 
-
-
+export const sectorDoms = () => {
+	const sectorBorder = document.querySelectorAll(".sector-border");
+	const arc = document.querySelectorAll(".sector-arc");
+    const textPath = document.querySelectorAll(".text-path");
+	const borders = Array.from(sectorBorder);   
+    const arcs = Array.from(arc);
+    const text = Array.from(textPath);
+	return [borders, arcs, text];
+};
