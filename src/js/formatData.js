39ae62ec46d6412e928288994c11425b5c307f79
MichelleEmbleton <michellelembleton@gmail.com>
2019-06-01 11:43:14 +0100

    Update formatData.js

--- src/js/formatData.js
+++ src/js/formatData.js
@@ -1,11 +1,16 @@
 
-export const toLower = el => el = el.toLowerCase();
+
+export const format = el => {
+    el = el.toLowerCase();
+    if(!el.replace(/\s/g, "").length) el = "";  
+    return el;    
+};   
 
 export const getUniqCats = data => { 
     const sectors = [];       
     data.forEach(el => {
         if(el.CAT && sectors.indexOf(el.CAT) === -1){
-            sectors.push(el.CAT);				
+            el.CAT !== " " && sectors.push(el.CAT);				
         }
     });
     return sectors;
@@ -19,14 +24,23 @@ export const getUniqSubcats = subcatConfig => {
     return subcats;
 };
 
+export const setRadii = (el, config) => {
+    for(let i = 0; i < config.length; i++){
+        if(el.STATUS && el.STATUS === config[i].TITLE){
+            el.radius = Number(config[i].RADIUS); 
+            el.minRadius = Number(config[i].minRadius); 
+        }
+    };
+};
+
 export const configData = (data, config, subcatConfig) => {                                     
     data.forEach(el => {   
         for(let i = 0; i < config.length; i++){
             if(el.STATUS && el.STATUS === config[i].TITLE){
-                el.statusId = config[i].ID;     
-                el.radius = Number(config[i].RADIUS); 
-                el.minRadius = Number(config[i].minRadius); 
+                el.statusId = config[i].ID;  
             }
+            setRadii(el, config);
+            el.colorId = !el.CAT ? 'c0' : el.statusId;
         }; 
         if(subcatConfig){                
             for(let i = 0; i < subcatConfig.length; i++){
@@ -35,5 +49,6 @@ export const configData = (data, config, subcatConfig) => {
                 }	
             };
         }
+        if(!el.subcatId) el.subcatId = '0';    
     });
 };
