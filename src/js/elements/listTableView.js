950050d82da6a7cbf2d33f595ed84088759302b4
MichelleEmbleton <michellelembleton@gmail.com>
2019-06-03 07:51:08 +0100

    Update listTableView.js

--- src/js/elements/listTableView.js
+++ src/js/elements/listTableView.js
@@ -1,8 +1,7 @@
-import { doms } from '../base';
 import '../../css/tables.css';
 
-const renderTable = (heading, titles, data, element) => {
-    const renderTitles = title => `<th>${title}</th>`;
+export const renderTable = (heading, titles, data, element) => {
+    let renderTitles = title => data.length > 0 ? `<th>${title}</th>` : `<td></td>`;
     const renderData = el => `
         <tr>
             <td class="${el.bkColor}">${el.td1}</td>
@@ -19,68 +18,4 @@ const renderTable = (heading, titles, data, element) => {
     </table> `;
 
     element.insertAdjacentHTML("afterbegin", tableContent);
-};
-
-export const createSectorTable = (headings, titles, data) => {   
-    const element = doms.sectorList;
-    headings.forEach(heading => {       
-        const sectorData = [];
-        for(let i = 0; i < data.length; i++){
-            data[i].cat === heading &&                               
-                sectorData.push({
-                    td1: data[i].tech,
-                    td2: data[i].subcat || "Unallocated",
-                    td3: data[i].status || "Unallocated",
-                    td4: data[i].details || "No Details",
-                    bkColor: data[i].statusId || "nostatus"
-                });                                     
-        }; 
-        sectorData.forEach(el => {
-            if(el.td2 === "Unallocated" || el.td2.includes("Invalid")) el.bkColor = "nosubcat";
-        });          
-        renderTable(heading, titles, sectorData, element);
-    }); 
-};     
-
-export const createStatusTable = (headings, titles, data) => {
-    const element = doms.statusList;
-    headings.forEach(heading => {       
-        const statusData = [];
-        for(let i = 0; i < data.length; i++){
-            data[i].status === heading &&                               
-                statusData.push({
-                    td1: data[i].tech,
-                    td2: data[i].cat || "Unallocated",
-                    td3: data[i].subcat || "Unallocated",
-                    td4: data[i].details || "No Details",
-                    bkColor: data[i].statusId
-                });                   
-        }; 
-        statusData.forEach(el => {
-            if(el.td2 === "Unallocated") el.bkColor = "nocat";
-            if(el.td3 === "Unallocated" || el.td3.includes("Invalid")) el.bkColor = "nosubcat";
-        });         
-        renderTable(heading, titles, statusData, element);
-    });           
-};
-
-export const createSubcatTable = (headings, titles, data) => {
-    const element = doms.subcatList;
-    headings.forEach(heading => {   
-        const subcatData = [];
-        for(let i = 0; i < data.length; i++){
-            data[i].subcat === heading &&                               
-                subcatData.push({
-                    td1: data[i].tech,
-                    td2: data[i].cat || "Unallocated",
-                    td3: data[i].status || "Unallocated",
-                    td4: data[i].details || "No details",
-                    bkColor: data[i].statusId || "nostatus"
-                });                   
-        }; 
-        subcatData.forEach(el => {
-            if(el.td2 === "Unallocated") el.bkColor = "nocat";
-        });            
-        renderTable(heading, titles, subcatData, element);
-    });           
 };
