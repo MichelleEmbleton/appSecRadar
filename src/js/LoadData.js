4c35dba9fa124f18cbaaefdaa9860bad0c652b05
MichelleEmbleton <michellelembleton@gmail.com>
2019-06-03 07:59:07 +0100

    Update LoadData.js

--- src/js/LoadData.js
+++ src/js/LoadData.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { doms } from './base';
 
 export default class LoadData {
 
@@ -8,11 +9,16 @@ export default class LoadData {
             this.data = res.data;
         }
         catch(err){
-            console.log(`LoadData error at LoadData: ${err}`);
+            const dataError = `
+            Data isn't loading! <br />
+            Check data source: the fields should match the template JSON file.<br />
+            Check path in LoadData - the default is localhost:8080 - <br />
+            for the test data.json file in /dist - <br />
+            you need to change it to where your json data is coming from if not here.`;
+        doms.errorMessage.classList.add('error-show');
+        doms.errorMessage.innerHTML = dataError; 
+        console.log(err); 
         }       
     };
-}
+};
 
-
-
-	
