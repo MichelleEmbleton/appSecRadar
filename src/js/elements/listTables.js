d5ab5dc53d3b1dd0d59c442d2ac78755195b869e
MichelleEmbleton <michellelembleton@gmail.com>
2019-06-05 10:42:06 +0100

    Update listTables.js

--- src/js/elements/listTables.js
+++ src/js/elements/listTables.js
@@ -6,48 +6,46 @@ const columnTitles = (col2, col3) => {
 };
 
 export const tableControl = ({sign, btnId, data, headings, element}) => {   
-    if(sign === '+') clearView(element);
+    if(sign === '+') clearView(element);    
     else {
         if(data){
-            let listData;                            
+            let titles;                            
             headings.forEach(heading => {  
                 const tableData = [];
                 for(let i = 0; i < data.length; i++){
-                    if(btnId === 'sector'){
-                        listData = {
+                    const tableConfig = {
+                        sector: {
                             columns: columnTitles('subcategory', 'status'),
                             listBy: data[i].CAT,
                             td2: data[i].SUBCAT,
                             td3: data[i].STATUS
-                        }
-                    }
-                    if(btnId === 'status'){
-                        listData = {
+                        },
+                        status: {
                             columns: columnTitles('category', 'subcategory'),
                             listBy: data[i].STATUS,
                             td2: data[i].CAT,
                             td3: data[i].SUBCAT
-                        }               
-                    }
-                    if(btnId === 'subcat'){
-                        listData = {
+                        },
+                        subcat: {
                             columns: columnTitles('category', 'status'),
                             listBy: data[i].SUBCAT,
                             td2: data[i].CAT,
                             td3: data[i].STATUS
-                        }
+                        }   
                     }
 
-                    listData.listBy === heading &&                       
+                    const {columns, listBy, td2, td3} = tableConfig[btnId];
+                    titles = columns;
+                    listBy === heading &&                       
                         tableData.push({
                             td1: data[i].TECH,
-                            td2: listData.td2,
-                            td3: listData.td3,
+                            td2: td2,
+                            td3: td3,
                             td4: data[i].DETAILS,
                             bkColor: data[i].bkColor
-                        });                                                                              
+                        });                                                                               
                 };             
-                renderTable(heading, listData.columns, tableData, element);
+                renderTable(heading, titles, tableData, element);
             });             
             fadeIn(element);
         }       
